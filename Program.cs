f45ca5151675907bf56dcf614bdbfc14420decf1
mateomartin21 <mateo.martin@tecdesoftware.edu.mx>
2026-06-03 07:27:23 -0600

    fix: interfaces y links implementados

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using CitasApp.Interfaces;
+using CitasApp.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<IPacienteRepository, JsonPacienteRepository>();
+builder.Services.AddScoped<IMedicoRepository, JsonMedicoRepository>();
+builder.Services.AddScoped<ICitaRepository, JsonCitaRepository>();
 
 var app = builder.Build();
 
