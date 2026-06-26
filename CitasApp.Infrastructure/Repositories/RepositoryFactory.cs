f021e3f6968afb98dff1eba5ed8e26c9938e90cf
mateomartin21 <mateo.martin@tecdesoftware.edu.mx>
2026-06-25 21:22:31 -0600

    feat: Factory, decorator y observer agregados

---
+++ CitasApp.Infrastructure/Repositories/RepositoryFactory.cs
@@ -0,0 +1,44 @@
+﻿using CitasApp.Domain.Interfaces;
+using Microsoft.AspNetCore.Hosting;
+
+namespace CitasApp.Infrastructure.Repositories
+{
+    public static class RepositoryFactory
+    {
+        public static IPacienteRepository CrearPacienteRepository(
+            string entorno, IWebHostEnvironment env)
+        {
+            var dataFolder = Path.Combine(env.ContentRootPath, "data");
+
+            return entorno switch
+            {
+                "Production" => new SqlitePacienteRepository(dataFolder),
+                _ => new JsonPacienteRepository(dataFolder)
+            };
+        }
+
+        public static IMedicoRepository CrearMedicoRepository(
+            string entorno, IWebHostEnvironment env)
+        {
+            var dataFolder = Path.Combine(env.ContentRootPath, "data");
+
+            return entorno switch
+            {
+                "Production" => new SqliteMedicoRepository(dataFolder),
+                _ => new JsonMedicoRepository(dataFolder)
+            };
+        }
+
+        public static ICitaRepository CrearCitaRepository(
+            string entorno, IWebHostEnvironment env)
+        {
+            var dataFolder = Path.Combine(env.ContentRootPath, "data");
+
+            return entorno switch
+            {
+                "Production" => new SqliteCitaRepository(dataFolder),
+                _ => new JsonCitaRepository(dataFolder)
+            };
+        }
+    }
+}
